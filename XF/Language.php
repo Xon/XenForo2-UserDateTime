27a6a730dd5944ed72da525279a8c28af67fcf82
Xon <635541+Xon@users.noreply.github.com>
2023-03-01 15:54:10 +0800

    Allow date/time formats to be extended by other add-ons & use php7+ syntax to get the custom field values

--- XF/Language.php
+++ XF/Language.php
@@ -2,15 +2,15 @@
 
 namespace CmptrWz\UserDateTime\XF;
 
-// We don't really use this, but need it for the $db parameter passthrough.
 use XF\Db\AbstractAdapter;
+use function array_key_exists;
 
 class Language extends XFCP_Language
 {
 	// We can't store the formats in the custom field keys, so we store them here instead.
 	// Most of these come from the XF2 Language date and time options, with the exception of the Y-m-d format.
 	// No custom format included, though that wouldn't be much more difficult to implement.
-	private $cw_date_formats = [
+	protected $cw_date_formats = [
 		'mon_day_year' => 'M j, Y',
 		'month_day_year' => 'F j, Y',
 		'day_mon_year' => 'j M Y',
@@ -21,37 +21,41 @@ class Language extends XFCP_Language
 		'mdyf' => 'n/j/Y',
 		'ymd' => 'Y-m-d'
 	];
-	private $cw_time_formats = [
+	protected $cw_time_formats = [
 		'12h' => 'g:i A',
 		'24h' => 'H:i'
 	];
 
-	static $cwSeenUserProfile = [];
+	protected static $cwSeenUserProfile = [];
 
 	// To actually change the date and time formats, we just need to override initialization.
 	public function __construct($id, array $options, AbstractAdapter $db, $groupPath, array $phrases = null)
 	{
 		// Fetch the visitor
 		$visitor = \XF::visitor();
-		$userId = $visitor->user_id;
+		$userId = (int)$visitor->user_id;
 		$profile = $visitor->Profile;
 		// Check if they're logged in
-		if ($userId && empty(self::$cwSeenUserProfile[$userId]) && $profile)
+		if ($userId !== 0 && $profile !== null && array_key_exists($userId, static::$cwSeenUserProfile))
 		{
-			self::$cwSeenUserProfile[$userId] = true;
+			static::$cwSeenUserProfile[$userId] = true;
 			// Look for our settings in their custom fields
 			$customFields = $profile->custom_fields;
 			// We only override each of these if the value is set to something we have a format string for
 			// Otherwise we leave the relevant setting untouched so things can fall through
-			if (!empty($customFields->cw_user_date) && !empty($this->cw_date_formats[$customFields->cw_user_date]))
+			$userDate = $this->cw_date_formats[$customFields->getFieldValue('cw_user_date')] ?? null;
+			if ($userDate !== null)
 			{
-				$options['date_format'] = $this->cw_date_formats[$customFields->cw_user_date];
+				$options['date_format'] = $userDate;
 			}
-			if (!empty($customFields->cw_user_time) && !empty($this->cw_time_formats[$customFields->cw_user_time]))
+
+			$userTime = $this->cw_time_formats[$customFields->getFieldValue('cw_user_time')] ?? null;
+			if ($userTime !== null)
 			{
-				$options['time_format'] = $this->cw_time_formats[$customFields->cw_user_time];
+				$options['time_format'] = $userTime;
 			}
 		}
+
 		// Now that we're done potentially tweaking $options, resume normal service.
 		parent::__construct($id, $options, $db, $groupPath, $phrases);
 	}
